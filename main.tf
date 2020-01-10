bc530724f2947cd8e0f60e8c6bf8a53618a2d91d
doug-forester <40618118+doug-forester@users.noreply.github.com>
2020-01-10 12:48:31 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,15 +14,14 @@ provider "google-beta" {
 module "bb-host" {
   source           = "buildingbridge/terraform-google-hosts"
   version          = "0.1.1"
-  credentials_file = "auth-file-123456678.json"
+  credentials_file = "GoogleCloudTesting-97dd385312f1.json"
   region           = "${var.region}"
   zone             = "${var.region}-a"
   project          = "${var.project}"
   domain           = "buildingbridge.com"
   dns_zone         = "buildingbridge"
   public_ip        = "1.2.3.4"
-  certificate      = "https://www.googleapis.com/compute/v1/project/terraform-test/global/sslCertificates/tfe"
-  subnet           = "tfe-subnet"
+  subnet           = "bb-host-subnet"
 }
 
 output "bb-host" {
@@ -34,3 +33,28 @@ output "bb-host" {
     encryption_password          = "${module.bb-host.encryption_password}"
   }
 }
+  
+  
+resource "google_compute_instance" "vm_instance" {
+  name         = "bb-host-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "buildingbridge/bb-default-host-image"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "${google_compute_network.vpc_network.self_link}"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "bb-host-network"
+  auto_create_subnetworks = "true"
+}
+
