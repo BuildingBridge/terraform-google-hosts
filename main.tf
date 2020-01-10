68e286c19c206bda3d8d15957d176cfb17472246
doug-forester <40618118+doug-forester@users.noreply.github.com>
2020-01-10 11:38:39 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-provider "google" {
+provider "google-beta" {
   credentials = "${file("account.json")}"
-  project     = "my-project-id"
-  region      = "us-central1"
+  project     = "rising-goal-259700 "
+  region      = "us-west1-b"
 }
 
